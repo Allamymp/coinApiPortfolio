30de69459e75d63611ab68f767d38adac58950e9
Allamymp <allamympereira@gmail.com>
2024-04-18 12:19:09 -0300

    FEATURE:  log4j2 added

--- src/main/java/com/portfolio/coinapi/CoinApiApplication.java
+++ src/main/java/com/portfolio/coinapi/CoinApiApplication.java
@@ -1,5 +1,9 @@
 package com.portfolio.coinapi;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,5 +19,6 @@ public class CoinApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoinApiApplication.class, args);
     }
+
 
 }
